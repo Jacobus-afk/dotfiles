fd8e206a2650d3def3337c933d90e178ffacb68b
Jacobus-afk <s24162826@gmail.com>
2024-05-20 21:24:58 +0200

    added gnome-terminal profile details

--- quickstow.sh
+++ quickstow.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+echo 1. Apply dotfiles
+echo 2. Apply gnome-terminal profiles
+echo :
+read -r answer
 
-stow -S nvim tmux zsh
+if [[ $answer -eq 1 ]]; then
+    echo Applying dotfiles..
+    stow -S nvim tmux zsh
+elif [[ $answer -eq 2 ]]; then
+    echo Applying gnome-terminal profiles
+    dconf load /org/gnome/terminal/legacy/profiles:/ <gnome-terminal-profiles.dconf
+fi

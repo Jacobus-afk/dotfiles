05a0e6f1ed32064b64cd4ce1e1b52aee1c3597c8
Jacobus-afk <s24162826@gmail.com>
2024-05-31 22:22:04 +0200

    updated branch with latest from master

--- quickstow.sh
+++ quickstow.sh
@@ -6,7 +6,7 @@ read -r answer
 
 if [[ $answer -eq 1 ]]; then
     echo Applying dotfiles..
-    stow -S nvim tmux zsh
+    stow -S nvim tmux zsh alacritty
 elif [[ $answer -eq 2 ]]; then
     echo Applying gnome-terminal profiles
     dconf load /org/gnome/terminal/legacy/profiles:/ <gnome-terminal-profiles.dconf

ffeff17737d502e6bce4097eabcd4d774838f90d
Cobus Linde <barendl@staycontactable.com>
2024-07-13 21:32:37 +0200

    lazyvim keymap change - c doesn't copy to system clipboard

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap("n", "c", '""c', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "c", '""c', { noremap = true, silent = true })

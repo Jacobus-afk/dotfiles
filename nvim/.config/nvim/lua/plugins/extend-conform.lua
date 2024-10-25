46ada6603a605a6d0dd8fd151128821624d81dbd
Cobus Linde <barendl@staycontactable.com>
2024-10-25 19:38:52 +0200

    djhtml as formatter for htmldjango

--- nvim/.config/nvim/lua/plugins/extend-conform.lua
+++ nvim/.config/nvim/lua/plugins/extend-conform.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         ["python"] = { "ruff_organize_imports", "black" },
-        ["htmldjango"] = { "djlint" },
+        ["htmldjango"] = { "djhtml" },
       },
       formatters = {
         black = {
@@ -13,6 +13,29 @@ return {
         ruff_organize_imports = {
           append_args = { "--line-length", "110" },
         },
+        djhtml = {
+          inherit = false,
+          command = "djhtml",
+          args = {
+            "$FILENAME",
+            "--tabwidth",
+            "2",
+          },
+          stdin = false,
+        },
+        -- djlint = {
+        --   append_args = {
+        --     "--reformat",
+        --     "--format-css",
+        --     "--format-js",
+        --     "--indent",
+        --     "2",
+        --     "--indent-css",
+        --     "2",
+        --     "--indent-js",
+        --     "2",
+        --   },
+        -- },
       },
     },
   },
